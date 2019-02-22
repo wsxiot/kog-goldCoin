633011708e13b561166c3cf4a36e3158596a9560
wsxiot <860223178@qq.com>
2019-02-22 15:43:21 +0800

    ui2.0

--- kog.py
+++ kog.py
@@ -1,13 +1,14 @@
 import logging
+import random
 import os
 from time import sleep
-# ---------------在此处设置参数--------
+# ---------------在此处设置参数（很重要很重要很重要）--------
 # 设备分辨率
-device_x, device_y = 1280, 720
+device_x, device_y = 1920, 1080
 
-# 时间间隔，单位秒：one(time)three(time)five(time)six(time)one
-# one，three，five, six是图片名，详情见./imgs目录
-step_wait = [30, 150, 2, 10]
+# 时间间隔，单位秒：one(time)two(time)three(time)one
+# one，two，three是图片名，详情见./imgs目录
+step_wait = [180, 2, 10]
 
 # 刷金币次数
 repeat_times = 60
@@ -25,32 +26,27 @@ def tap_screen(x, y):
     real_x = int(x / base_x * device_x)
     real_y = int(y / base_y * device_y)
     os.system('adb shell input tap {} {}'.format(real_x, real_y))
+    logging.info('tap location {} {}'.format(real_x, real_y))
 
 def do_money_work(current_times):
                  
-    # step one 0
-    tap_screen(1466, 920)
-    for i in range(step_wait[0], 0, -1):
+    # click 闯关
+    tap_screen(random.randint(1336,1540), random.randint(852,898))
+    for i in range(step_wait[0]+random.randint(1,40), 0, -1):
         sleep(1)
         logging.info('round #{} step #{} time down #{}'.format(current_times, 0, i))
     
-    # step two 1
-    for i in range(step_wait[1], 0, -1):
-        tap_screen(1860, 92)
+    # click 点击屏幕继续
+    tap_screen(random.randint(200,1720), random.randint(200,880))
+    for i in range(step_wait[1]+random.randint(1,5), 0, -1):
         sleep(1)
         logging.info('round #{} step #{} time down #{}'.format(current_times, 1, i))
     
-    # step three 2
-    tap_screen(960, 540)
-    for i in range(step_wait[2], 0, -1):
+    # click 再次挑战
+    tap_screen(random.randint(1528,1738), random.randint(972,1020))
+    for i in range(step_wait[2]+random.randint(1,6), 0, -1):
         sleep(1)
         logging.info('round #{} step #{} time down #{}'.format(current_times, 2, i))
-    
-    # step four 3
-    tap_screen(1606, 992)
-    for i in range(step_wait[3], 0, -1):
-        sleep(1)
-        logging.info('round #{} step #{} time down #{}'.format(current_times, 3, i))
 
 if __name__ == '__main__':
     for i in range(repeat_times):
